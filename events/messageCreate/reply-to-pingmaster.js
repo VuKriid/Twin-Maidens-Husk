8cb3e5dcd715900c7e41f834a47144ffeba18bf0
VuKriid <145047149+VuKriid@users.noreply.github.com>
2024-08-18 05:20:17 +0200

    Create reply-to-pingmaster.js

---
+++ events/messageCreate/reply-to-pingmaster.js
@@ -0,0 +1,7 @@
+module.exports = (message) => {
+    if(message.author.bot)  return;
+    
+    if(message.content === '<@932708550684323841>') {
+        message.reply('```\nLas Doncellas Gemelas alzan sus manos lentamente sugiriendo que esperes en silencio.\n```')
+    }
+}
